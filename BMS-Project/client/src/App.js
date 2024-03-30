550f354c9f59a26be26ca5f15a574d7f7da9d818
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-30 18:38:19 +0530

    Book My Show Project Setup - Client and Server

--- BMS-Project/client/src/App.js
+++ BMS-Project/client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './Pages/Home/index';
+import Login from './Pages/Login/index';
+import Register from './Pages/Register/index';
+
+// Styles
+import "./stylesheets/alignments.css";
+import "./stylesheets/custom.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/sizes.css";
+import "./stylesheets/theme.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
