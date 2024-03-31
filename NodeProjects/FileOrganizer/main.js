2c09583464b71a6f73e164c95ab1bbf41fa9c9e6
Tharun Balaji <tharunbalaji110@gmail.com>
2024-03-31 14:51:18 +0530

    Implemented new folder creation to store Organized Files

--- NodeProjects/FileOrganizer/main.js
+++ NodeProjects/FileOrganizer/main.js
@@ -1,4 +1,6 @@
 
+const fs = require('fs');
+const path = require('path');
 
 const inputArr = process.argv.slice(2);
 // console.log(inputArr);
@@ -7,6 +9,13 @@ const inputArr = process.argv.slice(2);
 // node main.js help
 
 const command = inputArr[0];
+
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
 
 switch (command) {
     case "tree":
@@ -24,11 +33,33 @@ switch (command) {
 }
 
 function treeFn(dirPath){
-    console.log("Tree command Implemented for" ,dirPath);
+    // console.log("Tree command Implemented for" ,dirPath);
+   
 }
 
 function organizeFn(dirPath){
-    console.log("Organize command Implemented for" ,dirPath);
+    // console.log("Organize command Implemented for" ,dirPath);
+
+    if ( dirPath === undefined){
+        console.log("Please 🙏 Enter the directory path");
+        return;
+    }
+
+    //checking if the directory exists
+    if ( !fs.existsSync(dirPath) ){
+        console.log("Please 🙏 Enter the correct directory path");
+        return;
+    }
+
+
+    // creating the directory to store the files
+    const DestinationPath = path.join(dirPath,"Organized_Files");
+    if (!fs.existsSync(DestinationPath)){
+        fs.mkdirSync(DestinationPath);
+    }
+
+
+
 }
 
 function helpFn(){
@@ -40,3 +71,5 @@ function helpFn(){
         `
     );
 }
+
+
