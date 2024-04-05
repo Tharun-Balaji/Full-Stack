c075833cb0ab289ea04a9c46a254fd1e735bf1da
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-05 20:11:46 +0530

    Made user a Protected Route and Created Loading Animation

--- BMS-Project/client/src/App.js
+++ BMS-Project/client/src/App.js
@@ -11,13 +11,34 @@ import "./stylesheets/custom.css";
 import "./stylesheets/form-elements.css";
 import "./stylesheets/sizes.css";
 import "./stylesheets/theme.css";
+import ProtectedRoute from './components/protectedRoute';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const {loading} = useSelector( (state) => state.loaders)
+  // console.log(loading)
+
   return (
+    
     <div>
+      {
+      loading && (
+        <div className="loader-parent">
+        <div className="loader"></div>
+      </div>
+      )
+    }
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
