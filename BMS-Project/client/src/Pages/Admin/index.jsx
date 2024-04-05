0d6eff6d7387b59e39e790886178056306496a74
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-06 04:44:24 +0530

    Created Admin Component

--- BMS-Project/client/src/Pages/Admin/index.jsx
+++ BMS-Project/client/src/Pages/Admin/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Tabs } from "antd";
+import PageTitle from '../../components/PageTitle'
 
 export default function Admin() {
   return (
     <div>
-      Admin
+      <PageTitle title = "Admin" />
+      <Tabs defaultActiveKey='movies' >
+        <Tabs.TabPane tab = "Movies" key= "movies" >
+          Movie List
+        </Tabs.TabPane>
+        <Tabs.TabPane tab = "Theatres" key = "theatres" >
+        Theatre List
+        </Tabs.TabPane>
+      </Tabs>
     </div>
   )
 }
