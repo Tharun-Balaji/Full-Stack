d877ae5996242f22cf4e5ef74c09b006b8946901
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-09 12:50:20 +0530

    created theatre Route

--- BMS-Project/server/index.js
+++ BMS-Project/server/index.js
@@ -7,6 +7,7 @@ require("./config/dbConfig");
 
 const userRoute = require("./Routes/userRoutes");
 const movieRoute = require("./Routes/movieRoutes");
+const theatreRoute = require("./Routes/theatreRoute");
 
 app.use(cors());
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 
 app.use("/api/movie", movieRoute);
+
+app.use("/api/theatre", theatreRoute);
 
 app.listen(8082,()=>{
     console.log("Server is running on port http://localhost:8082/");
