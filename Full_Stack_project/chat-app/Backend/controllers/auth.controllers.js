f94005795888be84c7bfbae664958d61acb330d1
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-16 21:24:53 +0530

    created controller functions for signUp, login, logout

---
+++ Full_Stack_project/chat-app/Backend/controllers/auth.controllers.js
@@ -0,0 +1,13 @@
+
+
+export function signUp(request,response){
+    console.log("signUp User");
+}
+
+export function login(request,response) {
+    console.log("login User");
+}
+
+export function logout(request,response) {
+    console.log("logout User");
+}
--- Full_Stack_project/chat-app/Backend/routes/auth.routes.js
+++ Full_Stack_project/chat-app/Backend/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
+import { login, logout, signUp } from "../controllers/auth.controllers";
 
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-  res.send("Login Route");
-});
+router.get("/signup", signUp);
 
+router.get("/login", login);
 
-
+router.get("/logout", logout);
 
 export default router;
