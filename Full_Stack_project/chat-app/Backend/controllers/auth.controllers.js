af47bf25ed50aa648607c26a5f4483ed0a882a4f
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-16 22:16:12 +0530

    Implemented signUp api with encrypted  password stored in DB

--- Full_Stack_project/chat-app/Backend/controllers/auth.controllers.js
+++ Full_Stack_project/chat-app/Backend/controllers/auth.controllers.js
@@ -1,17 +1,62 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
+export async function signUp(request, response) {
+  try {
+    const { fullName, username, password, confirmPassword, gender } =
+      request.body;
 
-export async function signUp(request,response){
-    try {
-        const { fullName, username, password, confirmPassword, gender } = request.body;
-    } catch (error) {
-        
+    if (password !== confirmPassword) {
+      return response.status(400).send("Passwords do not match");
     }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return response.status(400).json("User already exists");
+    }
+
+    // Hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // https://avatar-placeholder.iran.liara.run/
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "Male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+
+      return response.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return response.status(400).json({ error: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in SignUp controller =>", error.message);
+    return response.send(500).json({
+      error: "Internal Server Error",
+    });
+  }
 }
 
-export function login(request,response) {
-    console.log("login User");
+export function login(request, response) {
+  console.log("login User");
 }
 
-export function logout(request,response) {
-    console.log("logout User");
+export function logout(request, response) {
+  console.log("logout User");
 }
--- Full_Stack_project/chat-app/Backend/server.js
+++ Full_Stack_project/chat-app/Backend/server.js
@@ -1,23 +1,23 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-const PORT = process.env.PORT ||5000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    // root route for http://localhost:5000/
-    res.send('Hello World!');
-})
+app.use("/api/auth", authRoutes);
 
-app.use("/api/auth",authRoutes);
+// app.get("/", (req, res) => {
+//     // root route for http://localhost:5000/
+//     res.send('Hello World!');
+// })
 
-app.listen(PORT, function(){
-    connectToMongoDB();
-    console.log(`Server listening on port ${PORT}`)
+app.listen(PORT, function () {
+  connectToMongoDB();
+  console.log(`Server listening on port ${PORT}`);
 });
