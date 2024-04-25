41bd413c5fc9a3683adf0024229db503fe84917d
Tharun Balaji <tharunbalaji110@gmail.com>
2024-04-25 12:14:40 +0530

    SidBar component Created

---
+++ Full_Stack_project/chat-app/Frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,10 @@
+
+import {BiLogOut} from "react-icons/bi"
+
+export default function LogoutButton() {
+  return (
+    <div className="mt-auto " >
+      <BiLogOut />
+    </div>
+  )
+}
--- Full_Stack_project/chat-app/Frontend/src/pages/home/Home.jsx
+++ Full_Stack_project/chat-app/Frontend/src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0" >
+      <Sidebar />
     </div>
   )
 }
