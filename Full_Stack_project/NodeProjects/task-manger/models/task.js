e8fd003c08e40ae891bcb416291040928ae3ce23
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-04 14:20:47 +0530

    created Task model

---
+++ Full_Stack_project/NodeProjects/task-manger/models/task.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  name: { type: String },
+  completed: {
+    type: Boolean,
+  },
+});
+
+module.exports = mongoose.model('Task',TaskSchema);
