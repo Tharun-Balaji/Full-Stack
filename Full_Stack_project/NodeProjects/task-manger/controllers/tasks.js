8014dffe90725fb440bfa8cb22a1dc10149dc17f
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-09 09:57:46 +0530

    completed getAllTasks controller

--- Full_Stack_project/NodeProjects/task-manger/controllers/tasks.js
+++ Full_Stack_project/NodeProjects/task-manger/controllers/tasks.js
@@ -1,24 +1,37 @@
 
 
-function getAllTasks(req,res){
-    res.send("get all tasks");
+const Task = require("../models/task");
+
+async function getAllTasks(req,res){
+    try {
+        const task = await Task.find({});
+        res.status(200).json({task});
+    } catch (err) {
+        res.status(500).json({ msg: err });
+    }
 };
 
-function createTask(req,res){
-    res.json(req.body);
+async function createTask(req,res){
+    try {
+      const task = await Task.create(req.body);
+      res.status(201).json({task});
+    } catch (err) {
+      res.status(500).json({ msg: err });
+    }
+    
 };
 
 function getTask(req,res) {
     res.json({id:req.params.id});
-}
+};
 
 function updateTask(req,res) {
     res.send("update task");
-}
+};
 
 function deleteTask(req,res) {
     res.send("delete task");
-}
+};
 
 module.exports = {
     getAllTasks,
@@ -26,4 +39,4 @@ module.exports = {
     getTask,
     updateTask,
     deleteTask
-}
+};
