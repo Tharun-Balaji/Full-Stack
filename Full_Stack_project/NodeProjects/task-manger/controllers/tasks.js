03b884cdb6d6478057fec82dd78b6d5879b5d0c3
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-22 20:18:24 +0530

    Added Delete task controller

--- Full_Stack_project/NodeProjects/task-manger/controllers/tasks.js
+++ Full_Stack_project/NodeProjects/task-manger/controllers/tasks.js
@@ -38,7 +38,16 @@ function updateTask(req, res) {
 }
 
 async function deleteTask(req, res) {
-  res.send("delete task");
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) { 
+      return res.status(404).json({ msg:`No task with id ${taskID}` });
+    };
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 }
 
 module.exports = {
