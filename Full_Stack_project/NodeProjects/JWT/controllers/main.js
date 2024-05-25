8218792c989e1d0750fe7a108aa7fdbf5a7d5556
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-25 15:48:24 +0530

    Added JWT authentication in daschboard controller

--- Full_Stack_project/NodeProjects/JWT/controllers/main.js
+++ Full_Stack_project/NodeProjects/JWT/controllers/main.js
@@ -32,11 +32,28 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-        msg: `Hello Tharun`,
-        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
-    });
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) { 
+        throw new CustomAPIError("No token provided", 401);
+    };
+
+    const token = authHeader.split(' ')[1];
+    // console.log(token);
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const luckyNumber = Math.floor(Math.random() * 100);
+        res.status(200).json({
+            msg: `Hello ${decoded.username}`,
+            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+        });
+    } catch (error) {
+        throw new CustomAPIError("Invalid token", 401);
+    }
+
+    
 };
 
 module.exports = {
