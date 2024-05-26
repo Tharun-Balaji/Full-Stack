f279ed97e345923bdc597834752779db6cdd02fe
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-26 17:36:54 +0530

    Created conrollers

---
+++ Full_Stack_project/NodeProjects/jobs-API/controllers/auth.js
@@ -0,0 +1,13 @@
+
+const register = async (req, res) => {
+    res.send("Register User");
+};
+
+const login = (req, res) => {
+    res.send("Login User");
+};
+
+module.exports = {
+    register,
+    login
+};
