b6a490942df8457977b05a7cca65ad11dc5bb296
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-26 20:13:33 +0530

    Created User model

--- Full_Stack_project/NodeProjects/jobs-API/models/User.js
+++ Full_Stack_project/NodeProjects/jobs-API/models/User.js
@@ -1,3 +1,29 @@
 
 const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+        maxlength: 50,
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+        lowercase: true,
+        minlength: 3,
+        maxlength: 50,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please provide a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 6,
+        maxlength: 12,
+    }
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
