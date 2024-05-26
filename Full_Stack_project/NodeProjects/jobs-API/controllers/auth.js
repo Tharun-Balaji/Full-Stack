7eabc74510141808d56e56054d9bef12fdbc45b3
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-26 21:11:48 +0530

    Created register controller

--- Full_Stack_project/NodeProjects/jobs-API/controllers/auth.js
+++ Full_Stack_project/NodeProjects/jobs-API/controllers/auth.js
@@ -1,6 +1,19 @@
 
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+// const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-    res.send("Register User");
+    const {name,email,password} = req.body;
+   
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const tempUser = { name, email, password: hashedPassword };
+
+    const user = await User.create({ ...tempUser });
+    res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = (req, res) => {
--- Full_Stack_project/NodeProjects/jobs-API/models/User.js
+++ Full_Stack_project/NodeProjects/jobs-API/models/User.js
@@ -12,7 +12,6 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please provide an email"],
         unique: true,
-        lowercase: true,
         minlength: 3,
         maxlength: 50,
         match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please provide a valid email address"]
@@ -21,7 +20,6 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please provide a password"],
         minlength: 6,
-        maxlength: 12,
     }
 });
 
