558a6f9247fb1fbf17a011d238fb1b30797ce0bb
Tharun Balaji <tharunbalaji110@gmail.com>
2024-05-26 21:23:23 +0530

    Created JWT for register controller

--- Full_Stack_project/NodeProjects/jobs-API/controllers/auth.js
+++ Full_Stack_project/NodeProjects/jobs-API/controllers/auth.js
@@ -2,11 +2,14 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 // const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
-    res.status(StatusCodes.CREATED).json(user);
+    const token = jwt.sign({ UserId: user._id, name: user.name }, "jwtSecret", {
+        expiresIn: "30d",
+    });
+    res.status(StatusCodes.CREATED).json({user:{name:user.name},token});
 };
 
 const login = (req, res) => {
