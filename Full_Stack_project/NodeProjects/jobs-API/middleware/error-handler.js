dfe62e4e0adfb86932f9a8eeaf2057ad42d7149b
Tharun Balaji <tharunbalaji110@gmail.com>
2024-06-02 16:45:42 +0530

    Created custom Error for mongoose validation Error

--- Full_Stack_project/NodeProjects/jobs-API/middleware/error-handler.js
+++ Full_Stack_project/NodeProjects/jobs-API/middleware/error-handler.js
@@ -13,6 +13,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
 
+  if (err.name === "ValidationError") { 
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = 400
+  };
+
   if (err.code && err.code === 11000) { 
     customError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
     customError.statusCode = 400
