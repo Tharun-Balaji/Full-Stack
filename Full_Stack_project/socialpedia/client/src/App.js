3aa1a9f3352b401eb2b9840007be7a14a3f2efbe
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-01 10:42:48 +0530

    defined routes

---
+++ Full_Stack_project/socialpedia/client/src/App.js
@@ -0,0 +1,25 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+
+function Layout() {
+  const user = null;
+  const location = useLocation();
+
+  return user?.token ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />
+}
+function App() {
+  return (
+    <div className="w-full min-h-[100vh]">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/profile/:id' element={<Profile />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
