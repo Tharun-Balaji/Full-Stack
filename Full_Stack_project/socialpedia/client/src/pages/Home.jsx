d64ca07a60a206686e5f5aff1b9881e883657532
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-02 13:39:10 +0530

    Completed Posting section

--- Full_Stack_project/socialpedia/client/src/pages/Home.jsx
+++ Full_Stack_project/socialpedia/client/src/pages/Home.jsx
@@ -1,18 +1,34 @@
 
 import { useSelector } from 'react-redux';
-import { FriendsCard, ProfileCard, TopBar } from "../components";
+import {
+	FriendsCard,
+	ProfileCard,
+	TopBar,
+	CustomButton,
+  TextInput,
+  Loading,
+} from "../components";
 import { friends, requests } from "../assets/data";
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { NoProfile } from '../assets';
-import CustomButton from './../components/CustomButton';
-import { BsPersonFillAdd } from "react-icons/bs";
+import { BsPersonFillAdd, BsFiletypeGif } from "react-icons/bs";
+import { useForm } from 'react-hook-form';
+import { BiImages, BiSolidVideo } from "react-icons/bi";
 
 export default function Home() {
 
   const { user } = useSelector((state) => state.user);
   const [friendRequest, setFriendRequest] = useState(requests);
   const [suggestedFriends, setSuggestedFriends] = useState(friends);
+  const [errMsg, setErrMsg] = useState("");
+  const [file, setFile] = useState(null);
+  const [posting, setPosting] = useState(false);
+  
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handlePostSubmit = (data) => { };
 
   return (
 		<div className="home w-full px-0 lg:px-10 pb-20 2xl:pb-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
@@ -24,7 +40,106 @@ export default function Home() {
 					<FriendsCard friends={user?.friends} />
 				</div>
 				{/*Center*/}
-				<div className="flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto"></div>
+				<div className="flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto rounded-lg">
+					<form
+						className="bg-primary px-4 rounded-lg"
+						onSubmit={handleSubmit(handlePostSubmit)}
+					>
+						<div className="w-full flex items-center gap-2 py-4 border-b border-[#66666645]">
+							<img
+								src={user?.profileUrl ?? NoProfile}
+								alt={user?.firstName}
+								className="w-14 h-14 object-cover rounded-full"
+							/>
+							<TextInput
+								type="text"
+								styles="w-full rounded-full py-5"
+								placeholder="What's on your mind.."
+								name="description"
+								register={register("description", {
+									required: "write Something to Post",
+								})}
+								error={
+									errors?.description
+										? errors.description.message
+										: ""
+								}
+							/>
+						</div>
+						{errMsg?.message && (
+							<span
+								role="alert"
+								className={`text-sm ${
+									errMsg?.status === "failed"
+										? "text-[#f64949fe]"
+										: "text-[#2ba150fe]"
+								} mt-0.5`}
+							>
+								{errMsg?.message}
+							</span>
+						)}
+
+						<div className="flex items-center justify-between py-4">
+							<label
+								htmlFor="imgUpload"
+								className="flex items-center gap-1 cursor-pointer text-base text-ascent-2 hover:to-ascent-1"
+							>
+								<input
+									type="file"
+									onChange={(e) => setFile(e.target.files[0])}
+									className="hidden"
+									id="imgUpload"
+									data-max-size="5120"
+									accept=".jpg, .png, .jpeg"
+								/>
+								<BiImages />
+								<span>Image</span>
+							</label>
+							<label
+								className="flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer"
+								htmlFor="videoUpload"
+							>
+								<input
+									type="file"
+									data-max-size="5120"
+									onChange={(e) => setFile(e.target.files[0])}
+									className="hidden"
+									id="videoUpload"
+									accept=".mp4, .wav"
+								/>
+								<BiSolidVideo />
+								<span>Video</span>
+							</label>
+
+							<label
+								className="flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer"
+								htmlFor="vgifUpload"
+							>
+								<input
+									type="file"
+									data-max-size="5120"
+									onChange={(e) => setFile(e.target.files[0])}
+									className="hidden"
+									id="vgifUpload"
+									accept=".gif"
+								/>
+								<BsFiletypeGif />
+								<span>Gif</span>
+							</label>
+							<div>
+								{posting ? (
+									<Loading />
+								) : (
+									<CustomButton
+										type="submit"
+										title="Post"
+										containerStyles="bg-[#0444a4] text-white py-1 px-6 rounded-full font-semibold text-sm"
+									/>
+								)}
+							</div>
+						</div>
+					</form>
+				</div>
 				{/*Right*/}
 				<div className="hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto">
 					{/*Friend Requests*/}
