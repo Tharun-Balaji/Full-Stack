3254483f76209160395c91c5b6cc4bca44c41bb8
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-11 10:36:47 +0530

    Added a controller for verficationEmail

---
+++ Full_Stack_project/socialpedia/server/controllers/userController.js
@@ -0,0 +1,80 @@
+
+import mongoose from "mongoose";
+import Verification from "../models/emailVerification.js";
+import Users from "../models/userModel.js";
+import { compareString } from "../utils/index.js";
+
+export const verifyEmail = async (req, res) => {
+
+  // check if user exists
+  const { userId, token } = req.params;
+
+  try {
+    const result = await Verification.findOne({ userId });
+
+    if (result) { // user exists
+
+      const { expiresAt, token: hashedToken } = result;
+
+      // token has expired
+      if (expiresAt < Date.now()) {
+        await Verification.findOneAndDelete({ userId }) // delete verification token
+          .then(() => {  // delete user
+            Users.findOneAndDelete({ _id: userId })
+              .then(() => {  // redirect
+                const message = "Verification token has expired.";
+                res.redirect(`/users/verified?status=error&message=${message}`);
+              })
+              .catch((err) => { // redirect
+                res.redirect(`/users/verified?status=error&message=`);
+              });
+          })
+          .catch((error) => { // redirect
+            console.log(error);
+            res.redirect(`/users/verified?message=`);
+          });
+
+      } else {
+        // check if token is valid
+        compareString(token, hashedToken)
+          .then((isMatch) => {
+            if (isMatch) { // token is valid
+              Users.findOneAndUpdate({ _id: userId }, { verified: true }) // update user
+                .then(() => { // delete verification token
+                  Verification.findOneAndDelete({ userId }).then(() => { // redirect
+                    const message = "Email verified successfully";
+                    res.redirect(
+                      `/users/verified?status=success&message=${message}`
+                    );
+                  });
+                })
+                .catch((err) => { // redirect
+                  console.log(err);
+                  const message = "Verification failed or link is invalid";
+                  res.redirect(
+                    `/users/verified?status=error&message=${message}`
+                  );
+                });
+            } else {
+              // invalid token
+              const message = "Verification failed or link is invalid";
+              res.redirect(`/users/verified?status=error&message=${message}`);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            res.redirect(`/users/verified?message=`);
+          })
+      }
+
+    } else { // verification link is invalid
+      const message = "Invalid verification link. Try again later.";
+      res.redirect(`/users/verified?status=error&message=${message}`);
+    }
+  } catch (error) {
+    console.log(err);
+    res.redirect(`/users/verified?message=`);
+  }
+
+};
+
