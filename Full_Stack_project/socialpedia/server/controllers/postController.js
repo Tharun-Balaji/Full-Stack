cbc04ee313bae007573594ed046283890cf925e3
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-13 17:26:38 +0530

    Create Conroller to getPosts Based on Search

--- Full_Stack_project/socialpedia/server/controllers/postController.js
+++ Full_Stack_project/socialpedia/server/controllers/postController.js
@@ -1,4 +1,5 @@
 
+import Users from '../models/userModel.js';
 import Posts from './../models/postModel.js';
 export const createPost = async (req, res, next) => {
 
@@ -35,4 +36,69 @@ export const createPost = async (req, res, next) => {
     res.status(404).json({ message: error.message });
   }
 
- };
+};
+ 
+export const getPosts = async (req, res, next) => { 
+  try {
+    // get user id
+    const { userId } = req.body.user;
+
+    // get search data
+    const { search } = req.body;
+
+    // find user
+    const user = await Users.findById(userId);
+
+    // get friends
+    const friends = user?.friends?.toString().split(",") ?? [];
+    friends.push(userId); // add user id to friends
+
+    // get search query
+    const searchPostQuery = {
+      $or: [ // or condition
+        {
+          description: { $regex: search, $options: "i" }, // case insensitive regex
+        },
+      ],
+    };
+
+    // get posts
+    const posts = await Posts.find(search ? searchPostQuery : {}) // search query
+      .populate({ // populate user
+        path: "userId",
+        select: "firstName lastName location profileUrl -password",
+      })
+      .sort({ _id: -1 }); // sort by id
+    
+    // get friends posts
+    const friendsPosts = posts?.filter((post) => {
+      return friends.includes(post?.userId?._id.toString());
+    });
+
+    // get other posts
+    const otherPosts = posts?.filter(
+      (post) => !friends.includes(post?.userId?._id.toString())
+    );
+
+    // create response
+    let postsRes = null;
+
+    // load friends posts first
+    if (friendsPosts?.length > 0) {
+      postsRes = search ? friendsPosts : [...friendsPosts, ...otherPosts];
+    } else {
+      postsRes = posts;
+    }
+
+    // send response
+    res.status(200).json({
+      success: true,
+      message: "successfully",
+      data: postsRes,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
--- Full_Stack_project/socialpedia/server/routes/postRoutes.js
+++ Full_Stack_project/socialpedia/server/routes/postRoutes.js
@@ -1,11 +1,18 @@
 
 import express from "express";
-import userAuth from "../middlewares/authMiddleware";
-import { createPost } from "../controllers/postController";
+import userAuth from "../middlewares/authMiddleware.js";
+import {
+  createPost,
+  getPosts
+} from "../controllers/postController.js";
 
 const router = express.Router();
 
 // crete post
 router.post("/create-post", userAuth, createPost);
+
+// get posts
+router.post("/", userAuth, getPosts);
+// router.post("/:id", userAuth, getPost);
 
 export default router;
