fa1537d142b5175d0c52df86ba9063d0c0fd9aaf
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-13 21:00:25 +0530

    Created a middleware to calculate how much time each request Takes

--- Full_Stack_project/socialpedia/server/index.js
+++ Full_Stack_project/socialpedia/server/index.js
@@ -9,6 +9,7 @@ import helmet from "helmet";
 import dbConnection from "./dbConfig/index.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import router from "./routes/index.js";
+import requestTimer from "./middlewares/requestTimer.js";
 
 
 const __dirname = path.resolve(path.dirname(""));
@@ -17,19 +18,35 @@ dotenv.config();
 
 const app = express();
 
+//request timer middleware
+app.use(requestTimer);
+
 app.use(express.static(path.join(__dirname, "views/build")));
 
 const PORT = process.env.PORT || 8800;
 
 dbConnection();
 
+// to Enhance security by setting various HTTP headers
 app.use(helmet());
+
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
+
+// Parse incoming request bodies in JSON format
 app.use(bodyParser.json());
+
+// Parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Built-in middleware to parse JSON bodies (replaces bodyParser.json())
+// Limits JSON payloads to 10MB
 app.use(express.json({ limit: "10mb" }));
+
+// Built-in middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
+// HTTP request logger middleware for development
 app.use(morgan("dev"));
 app.use(router);
 
