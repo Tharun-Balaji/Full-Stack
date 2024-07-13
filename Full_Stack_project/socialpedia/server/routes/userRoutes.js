30af15611d6efa241f8953dcc2bd575612fc5d22
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-13 15:42:58 +0530

    Created user Route

--- Full_Stack_project/socialpedia/server/routes/userRoutes.js
+++ Full_Stack_project/socialpedia/server/routes/userRoutes.js
@@ -1,15 +1,23 @@
 import express from "express";
 import path from "path";
-import { verifyEmail } from "../controllers/userController.js";
+import { changePassword, requestPasswordReset, resetPassword, verifyEmail } from "../controllers/userController.js";
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
 
+// email verification route
 router.get("/verify/:userId/:token", verifyEmail);
 
-
+// reset password route
+router.get("/reset-password/:userId/:token", resetPassword);
+router.post("/request-passwordreset", requestPasswordReset);
+router.post("/reset-password", changePassword);
 
 router.get("/verified", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/build", "index.html"));
+});
+
+router.get("/resetpassword", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/build", "index.html"));
 });
 
