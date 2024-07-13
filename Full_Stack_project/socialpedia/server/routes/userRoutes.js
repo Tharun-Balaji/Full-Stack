22dfa6b71420299a56efc117673440269e5028bd
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-13 16:03:51 +0530

    Created Routes for friend Requests

--- Full_Stack_project/socialpedia/server/routes/userRoutes.js
+++ Full_Stack_project/socialpedia/server/routes/userRoutes.js
@@ -22,6 +22,13 @@ router.post("/reset-password", changePassword);
 router.post("/get-user/:id?", userAuth, getUser);
 router.put("/update-user", userAuth, updateUser);
 
+// friend request
+router.post("/friend-request", userAuth, friendRequest);
+router.post("/get-friend-request", userAuth, getFriendRequest);
+
+// accept / deny friend request
+router.post("/accept-request", userAuth, acceptRequest);
+
 router.get("/verified", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/build", "index.html"));
 });
