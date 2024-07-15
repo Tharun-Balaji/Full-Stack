284ee312f00375396c40ebfd8524da5b094add8d
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-15 12:13:24 +0530

    Created a Function to Make API requests

--- Full_Stack_project/socialpedia/client/utils/index.js
+++ Full_Stack_project/socialpedia/client/utils/index.js
@@ -1,8 +1,42 @@
 import axios from "axios";
 
+// api url
 const API_URL = "http://localhost:8800";
 
+// axios instance
 export const API = axios.create({
   baseURL: API_URL,
   responseType: "json",
 });
+
+// api request
+export const apiRequest = async ({ url, method, data, token }) => { 
+
+  try {
+
+    const result = await API(url, { // make api request
+      method: method || "GET",
+      data: data ,
+      headers: {
+        "content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : "", // attach token
+      }
+    });
+
+    return result?.data;
+
+  } catch (error) { 
+
+    // destructure error
+    const err = error.response.data;
+
+    console.log(err);
+
+    // return error
+    return {
+      status: err.success,
+      message: err.message,
+    }
+  }
+};
+
