ac0cef12f453e82454a68e38085aa76bb4da26c3
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-19 20:20:56 +0530

    created user model

--- Full_Stack_project/mern-blog/server/index.js
+++ Full_Stack_project/mern-blog/server/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Listening on %i...", PORT);
 })
