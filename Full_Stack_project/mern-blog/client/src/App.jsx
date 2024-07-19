27fa3cd92adec1499620048d3e68b9d62d81b82a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-19 18:32:03 +0530

    created pages and routes

--- Full_Stack_project/mern-blog/client/src/App.jsx
+++ Full_Stack_project/mern-blog/client/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from './pages/SignUp';
+import DashBoard from './pages/DashBoard';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
-    <div className="text-3xl" >app</div>
-  )
+		<BrowserRouter>
+			<Routes>
+        <Route path="/"  component={<Home />} />
+        <Route path="/about" component={<About />} />
+        <Route path="/sign-in" component={<SignIn />} />
+        <Route path="/sign-up" component={<SignUp />} />
+        <Route path="/dashboard" component={<DashBoard />} />
+        <Route path="/projects" component={<Projects />} />
+			</Routes>
+		</BrowserRouter>
+  );
 }
