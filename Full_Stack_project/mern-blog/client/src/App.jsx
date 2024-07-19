05398925f7dfd0d6c095f744ecd8ad9ea49f3a4b
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-19 19:53:15 +0530

    created header section

--- Full_Stack_project/mern-blog/client/src/App.jsx
+++ Full_Stack_project/mern-blog/client/src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import SignIn from "./pages/SignIn"
 import SignUp from './pages/SignUp';
 import DashBoard from './pages/DashBoard';
 import Projects from './pages/Projects';
+import Header from "./components/Header";
 
 export default function App() {
   return (
-		<BrowserRouter>
+    <BrowserRouter>
+      <Header />
 			<Routes>
-        <Route path="/"  component={<Home />} />
-        <Route path="/about" component={<About />} />
-        <Route path="/sign-in" component={<SignIn />} />
-        <Route path="/sign-up" component={<SignUp />} />
-        <Route path="/dashboard" component={<DashBoard />} />
-        <Route path="/projects" component={<Projects />} />
+        <Route path="/"  element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/projects" element={<Projects />} />
 			</Routes>
 		</BrowserRouter>
   );
