78015eaace1a063782ed2df616dc5055bea60e81
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-19 20:15:49 +0530

    connected to database

--- Full_Stack_project/mern-blog/server/index.js
+++ Full_Stack_project/mern-blog/server/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.log(err);
+})
 
 const app = express();
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Listening on %s...", PORT);
+  console.log("Listening on %i...", PORT);
 })
