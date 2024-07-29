850c87bd51dc37843f6234bd2fd38090cfa6eb2b
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-29 18:26:31 +0530

    made the dashboard private

--- Full_Stack_project/mern-blog/client/src/App.jsx
+++ Full_Stack_project/mern-blog/client/src/App.jsx
@@ -1,26 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from './pages/SignUp';
-import DashBoard from './pages/DashBoard';
-import Projects from './pages/Projects';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
+	return (
+		<BrowserRouter>
+			<Header />
 			<Routes>
-        <Route path="/"  element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashBoard />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer />
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+				<Route element={<PrivateRoute />}>
+					<Route path="/dashboard" element={<DashBoard />} />
+				</Route>
+				<Route path="/projects" element={<Projects />} />
+			</Routes>
+			<Footer />
 		</BrowserRouter>
-  );
+	);
 }
