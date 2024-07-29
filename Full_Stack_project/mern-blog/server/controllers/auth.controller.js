4a770d2a40a5aa08d51f92c1c142620c762695b5
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-29 11:50:50 +0530

    created sigin api route

--- Full_Stack_project/mern-blog/server/controllers/auth.controller.js
+++ Full_Stack_project/mern-blog/server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export async function signUp(req, res, next) {
   // console.log(req.body);
@@ -35,4 +36,55 @@ export async function signUp(req, res, next) {
   } catch (error) {
     next(error);
   }
+};
+
+
+export async function signIn(req, res, next) { 
+
+  // get data from req.body
+  const { email, password } = req.body;
+
+  //check if all fields are filled
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+    return;
+  };
+
+  try {
+    
+    // find user
+    const validUser = await userModel.findOne({ email });
+
+    // if not valid user
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+      return;
+    };
+
+    // check password
+    const validPassword = await bcryptjs.compare(password, validUser.password);
+
+    // if not valid password
+    if (!validPassword) {
+      next(errorHandler(401, "Invalid password"));
+      return;
+    };
+
+    // create token
+    const token = jwt.sign({
+      id: validUser._id,
+    }, process.env.JWT_SECRET);
+
+    // delete password
+    validUser.password = undefined;
+
+    // send response
+    return res.status(200).cookie("access_token", token, { // set cookie
+      httpOnly: true // only accessible by web server
+    }).json(validUser)
+
+  } catch (error) {
+    next(error);
+  }
+
 };
