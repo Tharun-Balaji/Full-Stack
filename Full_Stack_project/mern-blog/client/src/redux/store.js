c93c34cde492f011730cc808b937fb8007fb16d8
Tharun Balaji <tharunbalaji110@gmail.com>
2024-07-29 12:46:33 +0530

    added redux-persist

--- Full_Stack_project/mern-blog/client/src/redux/store.js
+++ Full_Stack_project/mern-blog/client/src/redux/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
+import rootReducer from "./root";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
