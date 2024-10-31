cb7dbfab69ee5d76312086cec09a07dde98df0fc
Tharun Balaji <tharunbalaji110@gmail.com>
2024-10-31 20:54:36 +0530

    created post route

--- Full_Stack_project/mern-blog/server/routes/index.js
+++ Full_Stack_project/mern-blog/server/routes/index.js
@@ -1,8 +1,10 @@
 
 import authRoute from './auth.route.js';
 import userRoute from './user.route.js';
+import postRoute from './post.route.js';
 
 export {
   authRoute,
-  userRoute
+  userRoute,
+  postRoute
 };
