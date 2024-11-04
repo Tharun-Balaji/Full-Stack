ae1a1d55f74987b635bae6c78dbf155983958dcc
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-04 21:08:58 +0530

    added get user route

--- Full_Stack_project/mern-blog/server/routes/user.route.js
+++ Full_Stack_project/mern-blog/server/routes/user.route.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
-router.post('/signout',signout);
+router.post('/signout', signout);
+router.get('/getusers', verifyToken);
 
 export default router;
