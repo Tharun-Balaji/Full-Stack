22ca1e8a0b762f700b29d510d32a008fce92aa11
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-04 12:48:14 +0530

    added explanatory comments to firebase file

--- Full_Stack_project/mern-blog/client/src/firebase.js
+++ Full_Stack_project/mern-blog/client/src/firebase.js
@@ -1,17 +1,22 @@
-// Import the functions you need from the SDKs you need
+// Import the initializeApp function from the 'firebase/app' module
+// This function is used to initialize the Firebase app
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
+  // The API key for the Firebase project
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // The authentication domain for the Firebase project
   authDomain: 'mern-blog-d16dd.firebaseapp.com',
+  // The project ID for the Firebase project
   projectId: 'mern-blog-d16dd',
+  // The storage bucket for the Firebase project
   storageBucket: 'mern-blog-d16dd.appspot.com',
+  // The messaging sender ID for the Firebase project
   messagingSenderId: '149332250394',
+  // The app ID for the Firebase project
   appId: '1:149332250394:web:09fb254726a5653f44a6a3'
 };
 
-// Initialize Firebase
+// Initialize the Firebase app using the configuration
 export const app = initializeApp(firebaseConfig);
