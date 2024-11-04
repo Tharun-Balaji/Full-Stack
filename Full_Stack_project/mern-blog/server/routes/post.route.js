6f9f6d12dc8d6b52299afa3bfd3282240f3d62b0
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-04 20:56:07 +0530

    created update post api route

--- Full_Stack_project/mern-blog/server/routes/post.route.js
+++ Full_Stack_project/mern-blog/server/routes/post.route.js
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/create', verifyToken, create);
 router.get('/getposts', getPosts)
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
+router.put('/updatepost/:postId/:userId', verifyToken);
 
 export default router;
