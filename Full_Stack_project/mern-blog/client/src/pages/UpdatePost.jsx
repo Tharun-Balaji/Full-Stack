b7543141b8df06043bc0633a7d67f250111252a9
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-04 21:06:50 +0530

    added update post functionality

--- Full_Stack_project/mern-blog/client/src/pages/UpdatePost.jsx
+++ Full_Stack_project/mern-blog/client/src/pages/UpdatePost.jsx
@@ -84,14 +84,42 @@ export default function UpdatePost() {
 			setImageUploadProgress(null);
 			console.log(error);
 		}
-	};
+  };
+  
+   const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await fetch(
+				`/api/post/updatepost/${formData._id}/${currentUser._id}`,
+				{
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(formData),
+				}
+			);
+			const data = await res.json();
+			if (!res.ok) {
+				setPublishError(data.message);
+				return;
+			}
+
+			if (res.ok) {
+				setPublishError(null);
+				navigate(`/post/${data.slug}`);
+			}
+		} catch (error) {
+			setPublishError("Something went wrong");
+		}
+   };
 
 	return (
 		<div className="p-3 max-w-3xl mx-auto min-h-screen">
 			<h1 className="text-center text-3xl my-7 font-semibold">
 				Update post
 			</h1>
-			<form className="flex flex-col gap-4" >
+			<form className="flex flex-col gap-4" onSubmit={handleSubmit} >
 				<div className="flex flex-col gap-4 sm:flex-row justify-between">
 					<TextInput
 						type="text"
