2c8582e779f6571876d6d1c7fbdcf8aff49afdb0
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-04 18:44:34 +0530

    added show more functionality to result of the dashboard

--- Full_Stack_project/mern-blog/client/src/components/DashPosts.jsx
+++ Full_Stack_project/mern-blog/client/src/components/DashPosts.jsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 
 export default function DashPosts() {
 	const { currentUser } = useSelector((state) => state.user);
-	const [userPosts, setUserPosts] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
 
 
 	useEffect(() => {
@@ -16,7 +17,10 @@ export default function DashPosts() {
 				);
 				const data = await res.json();
 				if (res.ok) {
-					setUserPosts(data.posts);
+          setUserPosts(data.posts);
+          if (data.posts.length < 9) { // If there are less than 9 posts, set showMore to false
+            setShowMore(false);
+          }
 				}
 			} catch (error) {
 				console.log(error.message);
@@ -27,7 +31,25 @@ export default function DashPosts() {
 			// Only fetch posts if the user is an admin
 			fetchPosts();
 		}
-	}, [currentUser._id]);
+  }, [currentUser._id]);
+  
+    const handleShowMore = async () => {
+		const startIndex = userPosts.length;
+		try {
+			const res = await fetch(
+				`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+			);
+			const data = await res.json();
+			if (res.ok) {
+				setUserPosts((prev) => [...prev, ...data.posts]);
+				if (data.posts.length < 9) {
+					setShowMore(false);
+				}
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
 	return (
 		<div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -45,7 +67,7 @@ export default function DashPosts() {
 							</Table.HeadCell>
 						</Table.Head>
 						{userPosts.map((post) => (
-							<Table.Body className="divide-y">
+							<Table.Body key={post._id} className="divide-y">
 								<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
 									<Table.Cell>
 										{new Date(
@@ -72,8 +94,7 @@ export default function DashPosts() {
 									<Table.Cell>{post.category}</Table.Cell>
 									<Table.Cell>
 										<span
-											onClick={() => {
-											}}
+											onClick={() => {}}
 											className="font-medium text-red-500 hover:underline cursor-pointer"
 										>
 											Delete
@@ -91,6 +112,14 @@ export default function DashPosts() {
 							</Table.Body>
 						))}
 					</Table>
+					{showMore && (
+						<button
+							onClick={handleShowMore}
+							className="w-full text-teal-500 self-center text-sm py-7"
+						>
+							Show more
+						</button>
+					)}
 				</>
 			) : (
 				<p>You have no posts yet!</p>
