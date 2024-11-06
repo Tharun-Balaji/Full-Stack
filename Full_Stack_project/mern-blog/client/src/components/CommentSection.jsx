78f2c7c23a9b814efb53e4b6936c7bf60006e603
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-06 17:00:42 +0530

    added create comment functionality

--- Full_Stack_project/mern-blog/client/src/components/CommentSection.jsx
+++ Full_Stack_project/mern-blog/client/src/components/CommentSection.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Button, Textarea } from "flowbite-react";
+import { Alert, Button, Textarea } from "flowbite-react";
 import { useState } from "react";
 
 
@@ -8,6 +8,35 @@ export default function CommentSection({ postId }) {
 
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(null);
+
+  const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (comment.length > 200) {
+			return;
+		}
+		try {
+			const res = await fetch("/api/comment/create", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					content: comment,
+					postId,
+					userId: currentUser._id,
+				}),
+			});
+			const data = await res.json();
+			if (res.ok) {
+				setComment("");
+        setCommentError(null);
+			}
+		} catch (error) {
+			setCommentError(error.message);
+		}
+  };
+
 
   return (
 		<div className="max-w-2xl mx-auto w-full p-3">
@@ -38,7 +67,10 @@ export default function CommentSection({ postId }) {
 				</div>
 			)}
 			{currentUser && (
-				<form className="border border-teal-500 rounded-md p-3">
+				<form
+					onSubmit={handleSubmit}
+					className="border border-teal-500 rounded-md p-3"
+				>
 					<Textarea
 						placeholder="Add a comment..."
 						rows="3"
@@ -58,6 +90,11 @@ export default function CommentSection({ postId }) {
 							Submit
 						</Button>
 					</div>
+					{commentError && (
+						<Alert color="failure" className="mt-5">
+							{commentError}
+						</Alert>
+					)}
 				</form>
 			)}
 		</div>
