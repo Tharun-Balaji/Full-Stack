d9a7c89d4c61c1e1be89b830a19d78c592067801
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-06 16:38:39 +0530

    added comment route

--- Full_Stack_project/mern-blog/server/routes/index.js
+++ Full_Stack_project/mern-blog/server/routes/index.js
@@ -2,9 +2,11 @@
 import authRoute from './auth.route.js';
 import userRoute from './user.route.js';
 import postRoute from './post.route.js';
+import commentRoute from './comment.route.js';
 
 export {
   authRoute,
   userRoute,
-  postRoute
+  postRoute,
+  commentRoute
 };
