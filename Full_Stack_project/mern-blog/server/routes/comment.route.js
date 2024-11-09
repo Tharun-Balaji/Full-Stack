77bbefc8cdbd4edb8871dbde68963264cb7ed99a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-09 16:32:35 +0530

    added get comments route

--- Full_Stack_project/mern-blog/server/routes/comment.route.js
+++ Full_Stack_project/mern-blog/server/routes/comment.route.js
@@ -8,6 +8,7 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
-router.delete('/deleteComment/:commentId', verifyToken,deleteComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken);
 
 export default router;
