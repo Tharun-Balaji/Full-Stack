8ac0d5ab102f59a978d9e7aba4c4a938b8ca7f09
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-10 09:42:06 +0530

    created search page

--- Full_Stack_project/mern-blog/client/src/pages/Search.jsx
+++ Full_Stack_project/mern-blog/client/src/pages/Search.jsx
@@ -1,7 +1,74 @@
 
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
 
 export default function Search() {
-  return (
-    <div>Search</div>
-  )
+
+   const [sidebarData, setSidebarData] = useState({
+		searchTerm: "",
+		sort: "desc",
+		category: "uncategorized",
+   });
+  
+   const [posts, setPosts] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const [showMore, setShowMore] = useState(false);
+
+   const location = useLocation();
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+     
+		// Get the URL parameters from the current URL
+		const urlParams = new URLSearchParams(location.search);
+		const searchTermFromUrl = urlParams.get("searchTerm");
+		const sortFromUrl = urlParams.get("sort");
+    const categoryFromUrl = urlParams.get("category");
+
+		// If any of the URL parameters have changed, update the state
+		if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+			setSidebarData({
+				...sidebarData,
+				searchTerm: searchTermFromUrl,
+				sort: sortFromUrl,
+				category: categoryFromUrl,
+			});
+		}
+
+		// Fetch the posts from the server based on the current URL parameters
+		const fetchPosts = async () => {
+			setLoading(true);
+			// Create the search query string from the URL parameters
+			const searchQuery = urlParams.toString();
+			// Fetch the posts from the server
+			const res = await fetch(`/api/post/getposts?${searchQuery}`);
+			if (!res.ok) {
+				// If the request failed, stop loading and return
+				setLoading(false);
+				return;
+			}
+			if (res.ok) {
+				// If the request was successful, update the state with the posts
+				// and set the loading state to false
+				const data = await res.json();
+				setPosts(data.posts);
+				setLoading(false);
+				// If there are 9 posts, show the "Show More" button
+				if (data.posts.length === 9) {
+					setShowMore(true);
+				} else {
+					// Otherwise, hide the "Show More" button
+					setShowMore(false);
+				}
+			}
+		};
+		fetchPosts();
+	}, [location.search]);
+	
+
+
+
+  return <div>Search</div>;
 }
--- Full_Stack_project/mern-blog/client/src/pages/index.js
+++ Full_Stack_project/mern-blog/client/src/pages/index.js
@@ -8,6 +8,7 @@ import DashBoard from './DashBoard.jsx';
 import CreatePost from './CreatePost.jsx';
 import UpdatePost from './UpdatePost.jsx';
 import PostPage from './PostPage.jsx';
+import Search from './Search.jsx';
 
 export {
   Home,
@@ -18,5 +19,6 @@ export {
   DashBoard,
   CreatePost,
   UpdatePost,
-  PostPage
+  PostPage,
+  Search
 };
