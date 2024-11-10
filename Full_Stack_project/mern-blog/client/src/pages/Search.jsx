9e17aeefa1da5fa58b70ed8762e0387cad2f29df
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-10 09:46:34 +0530

    created search side bar

--- Full_Stack_project/mern-blog/client/src/pages/Search.jsx
+++ Full_Stack_project/mern-blog/client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Button, Select, TextInput } from 'flowbite-react';
 
 
 export default function Search() {
@@ -68,7 +69,62 @@ export default function Search() {
 	}, [location.search]);
 	
 
+	const handleSubmit = (e) => { };
+
+	const handleChange = (e) => { };
 
 
-  return <div>Search</div>;
+
+	return (
+		<div className="flex flex-col md:flex-row">
+			<div className="p-7 border-b md:border-r md:min-h-screen border-gray-500">
+				<form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+					<div className="flex   items-center gap-2">
+						<label className="whitespace-nowrap font-semibold">
+							Search Term:
+						</label>
+						<TextInput
+							placeholder="Search..."
+							id="searchTerm"
+							type="text"
+							value={sidebarData.searchTerm}
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="flex items-center gap-2">
+						{" "}
+						<label className="font-semibold">Sort:</label>
+						<Select
+							onChange={handleChange}
+							value={sidebarData.sort}
+							id="sort"
+						>
+							<option value="desc">Latest</option>
+							<option value="asc">Oldest</option>
+						</Select>
+					</div>
+					<div className="flex items-center gap-2">
+						<label className="font-semibold">Category:</label>
+						<Select
+							onChange={handleChange}
+							value={sidebarData.category}
+							id="category"
+						>
+							<option value="uncategorized">Uncategorized</option>
+							<option value="reactjs">React.js</option>
+							<option value="nextjs">Next.js</option>
+							<option value="javascript">JavaScript</option>
+						</Select>
+					</div>
+					<Button
+						type="submit"
+						outline
+						gradientDuoTone="purpleToPink"
+					>
+						Apply Filters
+					</Button>
+				</form>
+			</div>
+		</div>
+	);
 }
