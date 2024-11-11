1edcb630cc520cb9e6c08f2e7986e8b06f700b0a
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-11 20:54:21 +0530

    added find socket

--- Full_Stack_project/TIC-TAC-TOE/index.js
+++ Full_Stack_project/TIC-TAC-TOE/index.js
@@ -15,6 +15,52 @@ const io = new Server(server);
 // Serve static files from the directory specified
 app.use(express.static(path.resolve('')));
 
+const arr = []; // Array to store the names of players that are waiting to play
+const playingArray = []; // Array to store the objects of the players that are currently playing
+
+io.on('connection', (socket) => {
+
+  // Listen for the "find" event, which is emitted when a player wants to find another player
+  socket.on('find', (e) => { 
+
+    // Check if the player has a valid name
+    if (e.name != null) {
+      arr.push(e.name) // Add the name to the array
+
+      // If there are at least two players waiting to play, create a game object
+      if (arr.length >= 2) {
+        const p1obj = {
+          p1name: arr[0], // Store the name of the first player
+          p1value: "X", // Store the symbol of the first player
+          p1move: "" // Store the move of the first player
+        }
+        const p2obj = {
+          p2name: arr[1], // Store the name of the second player
+          p2value: "O", // Store the symbol of the second player
+          p2move: "" // Store the move of the second player
+        }
+
+        // Create a game object that contains the information of the two players
+        const obj = {
+          p1: p1obj,
+          p2: p2obj,
+          sum: 1 // Store the sum of the moves made by the two players
+        }
+        playingArray.push(obj) // Add the game object to the array
+
+        // Remove the names of the two players from the array
+        arr.splice(0, 2)
+
+        // Emit the "find" event to all connected clients with the updated array of games
+        io.emit("find", { allPlayers: playingArray })
+      }
+     }
+
+  })
+
+
+})
+
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
   return res.sendFile('index.html'); // Send 'index.html' file as a response
--- Full_Stack_project/TIC-TAC-TOE/script.js
+++ Full_Stack_project/TIC-TAC-TOE/script.js
@@ -1,8 +1,39 @@
 
 
+// Hide all the elements that are not needed when the page first loads
 document.getElementById("loading").style.display = "none"
 document.getElementById("bigCont").style.display = "none"
 document.getElementById("userCont").style.display = "none"
 document.getElementById("oppNameCont").style.display = "none"
 document.getElementById("valueCont").style.display = "none"
-document.getElementById("whosTurn").style.display = "none"
+document.getElementById("whosTurn").style.display = "none";
+
+// Connect to the server using socket.io
+const socket = io();
+
+// Get the value of the name input field
+let name;
+
+// Add an event listener to the "Find" button
+document.getElementById('find').addEventListener("click", function () {
+  // Get the value of the name field
+  name = document.getElementById("name").value
+  // Display the name in the "You" field
+  document.getElementById("user").innerText = name
+  
+  // Check if the name is valid
+  if (name == null || name == '') {
+    alert("Please enter a name")
+  }
+  else {
+
+    // Send the name to the server
+    socket.emit("find", { name: name })
+
+    // Show the loading gif
+    document.getElementById("loading").style.display = "block"
+    // Disable the "Find" button
+    document.getElementById("find").disabled = true;
+
+  }
+})
