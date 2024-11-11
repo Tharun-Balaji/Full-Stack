8a2ae5dffe6eddc1cdc73dd4bdb29ac3c72b5c67
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-11 20:40:34 +0530

    added styles

---
+++ Full_Stack_project/TIC-TAC-TOE/script.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("loading").style.display = "none"
+document.getElementById("bigCont").style.display = "none"
+document.getElementById("userCont").style.display = "none"
+document.getElementById("oppNameCont").style.display = "none"
+document.getElementById("valueCont").style.display = "none"
+document.getElementById("whosTurn").style.display = "none"
