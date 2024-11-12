874598af4e109b72daec92be4ce909363d5e1fd7
Tharun Balaji <tharunbalaji110@gmail.com>
2024-11-12 19:26:46 +0530

    fixed typo in header

--- Full_Stack_project/mern-blog/client/src/components/Header.jsx
+++ Full_Stack_project/mern-blog/client/src/components/Header.jsx
@@ -76,12 +76,14 @@ export default function Header() {
 				</span>
 				Blog
 			</Link>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<TextInput
 					type="text"
 					placeholder="Search"
 					rightIcon={AiOutlineSearch}
 					className="hidden lg:inline"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
 				/>
 			</form>
 			<Button className="w-12 h-10 lg:hidden" color="gray" pill>
@@ -120,7 +122,9 @@ export default function Header() {
 							<Dropdown.Item>Profile</Dropdown.Item>
 						</Link>
 						<Dropdown.Divider />
-						<Dropdown.Item onClick={handleSignOut} >Sign out</Dropdown.Item>
+						<Dropdown.Item onClick={handleSignOut}>
+							Sign out
+						</Dropdown.Item>
 					</Dropdown>
 				) : (
 					<Link to="/sign-in">
